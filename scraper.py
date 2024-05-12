56b31989d8631f623f58675573a35cd0f1d01c2e
ruben-hutter <hutter.ruben@gmail.com>
2024-05-12 11:37:54 +0200

    Add function to filter from participants list. Change folder structure in data.

--- scraper.py
+++ scraper.py
@@ -1,35 +1,37 @@
-import sys
 import csv
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+import sys
 import time
 
-def get_flights(date, take_off_site):
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def get_flights(date, take_off_site, event_id):
     ranked_flights = {}
     base_url = f'https://www.xcontest.org/switzerland/en/flights/daily-score-pg/#filter[date]={date}@filter[country]=CH@filter[detail_glider_catg]=FAI3'
     driver = webdriver.Firefox()
     wait = WebDriverWait(driver, 10)
-    
+
     try:
         driver.get(base_url)
         max_list_id = get_max_list_id(wait)
         count = 1
-        for i in range(0, max_list_id+100, 100):
-            print(f'Processing first flights {i+1}-{i+100}...')
+        for i in range(0, max_list_id + 100, 100):
+            print(f'Processing first flights {i + 1}-{i + 100}...')
             if i != 0:
                 url = f'{base_url}@flights[start]={i}'
                 driver.get(url)
-                time.sleep(2) # TODO: refactor without sleep
+                time.sleep(2)  # TODO: refactor without sleep
             flights_table = wait.until(
                 EC.visibility_of_element_located((By.CLASS_NAME, 'XClist'))
             )
             flights_table_body = flights_table.find_element(By.TAG_NAME, 'tbody')
             flights = flights_table_body.find_elements(By.TAG_NAME, 'tr')
             for flight in flights:
-                count = save_relevant_flights(flight, take_off_site, ranked_flights, count)
+                count = save_relevant_flights(flight, take_off_site, ranked_flights, count, event_id)
         return ranked_flights
 
     except TimeoutException as e:
@@ -41,7 +43,7 @@ def get_flights(date, take_off_site):
         driver.quit()
 
 
-def save_relevant_flights(flight, take_off_site, ranked_flights, rank):
+def save_relevant_flights(flight, take_off_site, ranked_flights, rank, event_id):
     cells = flight.find_elements(By.TAG_NAME, 'td')
     take_off_time = cells[1].text.splitlines()[0]
     pilot_name = cells[2].text
@@ -59,9 +61,14 @@ def save_relevant_flights(flight, take_off_site, ranked_flights, rank):
         .find_element(By.CSS_SELECTOR, 'div:nth-child(1)')
         .get_attribute('title')
     )
+    # TODO: Just for testing, remove later
     if pilot_name in ranked_flights:
         print(f'Pilot {pilot_name} already ranked')
-    if launch_site == take_off_site and pilot_name not in ranked_flights:
+    if (
+            launch_site == take_off_site
+            and pilot_name not in ranked_flights
+            and pilot_is_competing(event_id, pilot_name)
+    ):
         ranked_flights[pilot_name] = {
             'rank': rank,
             'take_off_time': take_off_time,
@@ -73,6 +80,18 @@ def save_relevant_flights(flight, take_off_site, ranked_flights, rank):
         }
         return rank + 1
     return rank
+
+
+# TODO: get year in main
+def pilot_is_competing(event_id, pilot_name, year=2024):
+    # TODO: maybe change to txt file
+    with open(f'data/{year}/participants/{event_id}.csv', newline='') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            if row[0] == pilot_name:
+                return True
+    return False
 
 
 def get_max_list_id(wait):
@@ -90,14 +109,16 @@ def export_flights(flights, date, take_off_site):
     with open(f'output/{date}_{take_off_site}.csv', 'w') as f:
         f.write('Rank,Take off time,Pilot name,Take off site,Distance (km),Route Type,Points,Avg speed (km/h),Glider\n')
         for pilot_name, flight in flights.items():
-            f.write(f"{flight['rank']},{flight['take_off_time']},{pilot_name},{flight['distance']},{flight['route_type']},{flight['points']},{flight['avg_speed']},{flight['glider']}\n")
+            f.write(
+                f"{flight['rank']},{flight['take_off_time']},{pilot_name},{flight['distance']},{flight['route_type']},{flight['points']},{flight['avg_speed']},{flight['glider']}\n")
     print('Export complete!')
 
 
-def get_date_and_take_off_site(event_id):
+# TODO: get year in main
+def get_date_and_take_off_site(event_id, year=2024):
     # event_id in csv are integers from 1 to n
     event_id = int(event_id)
-    with open('data/events.csv', newline='') as f:
+    with open(f'data/{year}/events.csv', newline='') as f:
         reader = csv.reader(f)
         next(reader)
         for i, row in enumerate(reader):
@@ -117,7 +138,7 @@ def main():
         print('Event not found')
         sys.exit(1)
 
-    flights = get_flights(date, take_off_site)
+    flights = get_flights(date, take_off_site, event_id)
     # TODO: compare pilots with competing pilots
     export_flights(flights, date, take_off_site)
     # TODO: create nice pdf with the data
