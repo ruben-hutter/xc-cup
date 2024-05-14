6de8dc28750946b32bb517ba7ce88d2b4f54954c
ruben-hutter <hutter.ruben@gmail.com>
2024-05-14 11:34:14 +0200

    Change run script to check if conda is installed

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-conda run -n xc-cup-env python xc-cup-ranker.py $@
+# If conda installed, run with conda, else run with python
+if command -v conda &> /dev/null; then
+    conda run -n xc-cup-env python xc-cup-ranker.py $@
+else
+    python3 xc-cup-ranker.py $@
+fi
