e824cf2adfd0d0a5a6311500f768118dcb63eba8
ruben-hutter <hutter.ruben@gmail.com>
2024-05-15 14:57:05 +0200

    Change run script to check if env exists

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 
 # If conda installed, run with conda, else run with python
 if command -v conda &> /dev/null; then
-    conda run -n xc-cup-env python xc-cup-ranker.py $@
+    if conda env list | grep -q "xc-cup-env"; then
+        conda run -n xc-cup-env python xc-cup-ranker.py $@
+    fi
 else
     python3 xc-cup-ranker.py $@
 fi
