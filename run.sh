3eefdca4557ced1713cfe33897361d8626deef38
ruben-hutter <hutter.ruben@gmail.com>
2024-05-28 15:20:29 +0200

    Update run script to use xc-cup-env if present

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-# If conda installed, run with conda, else run with python
+# If conda installed, run with conda, else if `xc-cup-env` exists, run with venv,
+# else try to run with system python3
 if command -v conda &> /dev/null; then
     if conda env list | grep -q "xc-cup-env"; then
         conda run -n xc-cup-env python xc-cup-ranker.py $@
     fi
+elif [ -d "xc-cup-env" ]; then
+    source xc-cup-env/bin/activate
+    python xc-cup-ranker.py $@
+    deactivate
 else
     python3 xc-cup-ranker.py $@
 fi
