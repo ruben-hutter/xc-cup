8f873bc9f7d9db8a63dbb93dddb13741c9288082
ruben-hutter <hutter.ruben@gmail.com>
2024-06-03 20:01:40 +0200

    Add tests for get_participants()

--- tests/test_xc_cup_ranker.py
+++ tests/test_xc_cup_ranker.py
@@ -2,7 +2,6 @@ from unittest.mock import patch
 
 import xc_cup_ranker
 
-
 @patch.object(xc_cup_ranker, 'event_id', 1)
 def test_get_date_and_take_off_site_valid():
     date, take_off_site = xc_cup_ranker.get_date_and_take_off_site()
@@ -15,3 +14,11 @@ def test_get_date_and_take_off_site_invalid():
     date, take_off_site = xc_cup_ranker.get_date_and_take_off_site()
     assert date is None
     assert take_off_site is None
+
+
+@patch.object(xc_cup_ranker, 'event_id', 1)
+@patch.object(xc_cup_ranker, 'year', 2024)
+def test_get_participants():
+    participants = xc_cup_ranker.get_participants()
+    assert len(participants) == 3
+    assert "Ruben Hutter" in participants
