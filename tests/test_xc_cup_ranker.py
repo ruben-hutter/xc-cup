f0982715aab679ad06cd8198227cae929e9dbfda
ruben-hutter <hutter.ruben@gmail.com>
2025-02-11 21:17:12 +0100

    Fix test script to use tests/data/ instead of data/

--- tests/test_xc_cup_ranker.py
+++ tests/test_xc_cup_ranker.py
@@ -1,51 +1,56 @@
+from pathlib import Path
 from unittest.mock import patch
 
 import xc_cup_ranker
 
-@patch.object(xc_cup_ranker, 'event_id', 1)
-@patch.object(xc_cup_ranker, 'year', 2024)
+TEST_DATA_DIR = Path(__file__).parent / "data"
+
+@patch.object(xc_cup_ranker, "DATA_DIR", TEST_DATA_DIR)
+@patch.object(xc_cup_ranker, "event_id", 1)
+@patch.object(xc_cup_ranker, "year", 2024)
 def test_get_date_and_take_off_site_valid():
     date, take_off_site = xc_cup_ranker.get_date_and_take_off_site()
     assert date == "2024-04-06"
     assert take_off_site == "Monte Tamaro"
 
 
-@patch.object(xc_cup_ranker, 'event_id', 0)
-@patch.object(xc_cup_ranker, 'year', 2024)
+@patch.object(xc_cup_ranker, "event_id", 0)
+@patch.object(xc_cup_ranker, "year", 2024)
 def test_get_date_and_take_off_site_invalid():
     date, take_off_site = xc_cup_ranker.get_date_and_take_off_site()
     assert date is None
     assert take_off_site is None
 
 
-@patch.object(xc_cup_ranker, 'event_id', 1)
-@patch.object(xc_cup_ranker, 'year', 2024)
+@patch.object(xc_cup_ranker, "DATA_DIR", TEST_DATA_DIR)
+@patch.object(xc_cup_ranker, "event_id", 1)
+@patch.object(xc_cup_ranker, "year", 2024)
 def test_get_participants_valid():
     participants = xc_cup_ranker.get_participants()
     assert len(participants) == 3
     assert "Ruben Hutter" in participants
 
 
-@patch.object(xc_cup_ranker, 'event_id', -1)
-@patch.object(xc_cup_ranker, 'year', 2024)
+@patch.object(xc_cup_ranker, "event_id", -1)
+@patch.object(xc_cup_ranker, "year", 2024)
 def test_get_participants_event_id_invalid():
-    with patch('sys.exit') as mock_exit:
+    with patch("sys.exit") as mock_exit:
         xc_cup_ranker.get_participants()
         mock_exit.assert_called_once()
 
 
-@patch.object(xc_cup_ranker, 'event_id', 1)
-@patch.object(xc_cup_ranker, 'year', 2023)
+@patch.object(xc_cup_ranker, "event_id", 1)
+@patch.object(xc_cup_ranker, "year", 2023)
 def test_get_participants_year_invalid():
-    with patch('sys.exit') as mock_exit:
+    with patch("sys.exit") as mock_exit:
         xc_cup_ranker.get_participants()
         mock_exit.assert_called_once()
 
 
-@patch.object(xc_cup_ranker, 'event_id', 2)
-@patch.object(xc_cup_ranker, 'year', 2024)
+@patch.object(xc_cup_ranker, "event_id", 2)
+@patch.object(xc_cup_ranker, "year", 2024)
 def test_get_participants_list_empty():
-    with patch('sys.exit') as mock_exit:
+    with patch("sys.exit") as mock_exit:
         participants = xc_cup_ranker.get_participants()
         assert len(participants) == 0
         mock_exit.assert_called_once()
